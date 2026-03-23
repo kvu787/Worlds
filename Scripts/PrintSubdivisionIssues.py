974c974b4d7442cc3fdd22e844e3d2701fc85105
Kevin Vu <kevin.m.vu@gmail.com>
2026-03-23 12:55:29 -0700

    Ensure subd modifiers are using optimal display

--- Scripts/PrintSubdivisionIssues.py
+++ Scripts/PrintSubdivisionIssues.py
@@ -4,9 +4,14 @@ def check_subd_modifiers():
     for obj in bpy.data.objects:
         subd_modifiers = [m for m in obj.modifiers if type(m) is bpy.types.SubsurfModifier]
         if len(subd_modifiers) > 0:
-            if any(m.levels > 4 for m in subd_modifiers):
-                print("-----")
-                print(obj.name)
+            # if any(m.levels > 4 for m in subd_modifiers):
+            #     print("-----")
+            #     print(obj.name)
+            # if any(not m.show_only_control_edges for m in subd_modifiers):
+            #     print("-----")
+            #     print(obj.name)
+            for m in subd_modifiers:
+                m.show_only_control_edges = True
 
 if __name__ == "__main__":
     print("######### BEGIN")
